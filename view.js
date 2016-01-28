2d5ed1ea7fd824662f891cbd2f7f542c30c2a214
hansvana <hansvanarken@gmail.com>
2016-01-28 16:14:21 +0200

    view basics

--- view.js
+++ view.js
@@ -1,6 +1,14 @@
 (function(engine) {
 
-    engine.check();
+    var currState = engine.initialState;
+    var newState = engine.takeTurn(currState,0);
 
+    console.log(newState);
+    console.log(typeof newState.ending);
+    console.log(typeof newState.sheep);
+
+    if (typeof newState.ending !== null) {
+        console.log("Game over");
+    }
 
 })(engine);
