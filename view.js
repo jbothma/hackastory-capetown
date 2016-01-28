b56675bfde8107dc49359ce3c69c6762198d5702
hansvana <hansvanarken@gmail.com>
2016-01-28 18:35:43 +0200

    Silly animation

--- view.js
+++ view.js
@@ -1,30 +1,49 @@
 (function(engine) {
 
-    var currState = engine.initialState;
-
-    console.log(currState);
+    var currState = null;
 
     //currState = engine.takeTurn(currState,0);
+    function updateState() {
 
-    if (typeof(currState.nextTurn.context) !== 'undefined' &&
-        typeof(currState.nextTurn.options) !== 'undefined' )
-        updateView(currState.nextTurn.context,
-                    currState.nextTurn.options);
+        if (currState === null){
+            currState = engine.initialState;
+        } else {
+            currState = engine.takeTurn(currState, 0 );
+        }
 
+        if (typeof(currState.turn.context) !== 'undefined' &&
+            typeof(currState.turn.options) !== 'undefined')
+            updateView(currState.turn.context,
+                currState.turn.options);
+    }
+    updateState();
 
     function updateView(context, options) {
-        console.log(context);
+        console.log(context, options);
 
-        $qText = $('#questionText');
-        $qMedia = $('#questionMedia');
-        $aList = $('#answerList');
+        var $newCard = $('#mainCard').clone()
+            .attr('id','newCard')
+            .appendTo( $('body') )
+            .css('position', 'absolute')
+            .css('top', '1em')
+            .css('left', '100%')
+            .animate({left: '1em'}, 500, function() {
 
+
+                $('#mainCard').remove();
+                $newCard.attr('id','mainCard')
+                    .css('position','static');
+            });
+
+        var $qText = $newCard.find('.questionText');
+        var $qMedia = $newCard.find('.questionMedia');
+        var $aList = $newCard.find('.answerList');
+
+        console.log($qText, $qMedia, $aList);
         // SET DESCRIPTION TEXT
 
-        if (typeof(context.text) !== 'undefined') {
+        if (typeof(context.text) !== 'undefined')
             $qText.html(context.text);
-            console.log('did')
-        }
 
         // SET DESCRIPTION MEDIA
 
@@ -34,22 +53,34 @@
             if (context.media.type === "image")
                 $qMedia.append(
                     $('<img>').attr('src',context.media.url)
-                )
+                );
 
         // SET ANSWERS
 
         $aList.empty();
 
-        options.forEach( function(option){
+        var option;
+
+        Object.keys(options).forEach(function(n) {
+
+           option = options[n];
+
            $aList.append(
                 $('<li>').html(option.label)
                     .on('click', function() {
-                       console.log('click!' + option.)
+                       console.log('click!' + n)
+                        animate();
+                        updateState();
                     })
            );
         });
 
 
+    }
+
+    function animate() {
+        var $question = $('#question');
+        var $answer = $('#answer');
     }
 
 
