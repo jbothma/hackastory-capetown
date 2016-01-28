0e59ff6bf65136d63dfe56fac99a99aa11c09857
hansvana <hansvanarken@gmail.com>
2016-01-28 17:31:59 +0200

    Nothing important

--- view.js
+++ view.js
@@ -4,14 +4,53 @@
 
     console.log(currState);
 
-    if (typeof)
+    //currState = engine.takeTurn(currState,0);
 
-    currState = engine.takeTurn(currState,0);
+    if (typeof(currState.nextTurn.context) !== 'undefined' &&
+        typeof(currState.nextTurn.options) !== 'undefined' )
+        updateView(currState.nextTurn.context,
+                    currState.nextTurn.options);
 
-    console.log(newState);
 
-    if (typeof newState.ending !== null) {
-        console.log("Game over");
+    function updateView(context, options) {
+        console.log(context);
+
+        $qText = $('#questionText');
+        $qMedia = $('#questionMedia');
+        $aList = $('#answerList');
+
+        // SET DESCRIPTION TEXT
+
+        if (typeof(context.text) !== 'undefined') {
+            $qText.html(context.text);
+            console.log('did')
+        }
+
+        // SET DESCRIPTION MEDIA
+
+        $qMedia.empty();
+
+        if (typeof(context.media) !== 'undefined')
+            if (context.media.type === "image")
+                $qMedia.append(
+                    $('<img>').attr('src',context.media.url)
+                )
+
+        // SET ANSWERS
+
+        $aList.empty();
+
+        options.forEach( function(option){
+           $aList.append(
+                $('<li>').html(option.label)
+                    .on('click', function() {
+                       console.log('click!' + option.)
+                    })
+           );
+        });
+
+
     }
+
 
 })(engine);
